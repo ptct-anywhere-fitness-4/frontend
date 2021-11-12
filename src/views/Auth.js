99c7aa08511435714fdaad017c18565abe59ca71
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-11-11 21:14:04 -0500

    Chore: added comment

--- src/views/Auth.js
+++ src/views/Auth.js
@@ -6,6 +6,7 @@ import LogIn from '../components/LogIn';
 // in case u need reference https://www.vagaro.com/Login.aspx?enc=OcSkgjY9W03cadswvmCRyTkpx5L/QVAs0PbyWAiM8aoOAWTpsNV4YlKvJadiR+hxiVJtWEXUB3UmfoLRCfE9eUeQ6MHAGyCumJK4cHcULMrIHl+h2thA1iloh5Rf3zbC
 
 export default function Auth() {
+  /// you can delete all of this below. I just made this as a little example for out meeting.
   const [wantsRegister, setWantsRegister] = useState(false);
   return (
     <div>
