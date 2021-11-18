ab6babde87ed7dbc0452ac6f8126187ec2dc76b5
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-11-17 19:09:03 -0800

    added some functionality to login component

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
 import { LockClosedIcon } from '@heroicons/react/solid';
 
-export default function LogIn() {
+const initialData = {
+  username: "", 
+  password: "",
+};
+
+const LogIn = () => {
+//export default function LogIn() {
   //return <div>ME LOGIN COMPONENT</div>;
+  //?? Not sure what's wrong with my state
+  const [data, setInitialData] = useState(initialData);
+
+  const handleChanges = (e) => {
+    setInitialData({
+      ...data,
+      [e.target.name]: e.target.value,
+    
+    });
+  };
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+
   return (
    <>
      
@@ -19,7 +43,7 @@ export default function LogIn() {
             </h2>
             
           </div>
-          <form className='mt-8 space-y-6' action='#' method='POST'>
+          <form onSubmit={handleSubmit} className='mt-8 space-y-6' action='#' method='POST'>
             <input type='hidden' name='remember' defaultValue='true' />
             <div className='-space-y-px rounded-md shadow-sm'>
               <div>
@@ -27,9 +51,11 @@ export default function LogIn() {
                   Username
                 </label>
                 <input
+                  onChange={handleChanges}
                   id='email-address'
                   name='username'
                   type='username'
+                  value={data.username}
                   autoComplete='email'
                   required
                   className='relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
@@ -41,9 +67,11 @@ export default function LogIn() {
                   Password
                 </label>
                 <input
+                  onChange={handleChanges}
                   id='password'
                   name='password'
                   type='password'
+                  value={data.password}
                   autoComplete='current-password'
                   required
                   className='relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
@@ -94,8 +122,14 @@ export default function LogIn() {
       </div>
     </>
   );
+
+  const mapStateToProps = (state) => {
+    return {};
+  }
 }
 
+//export default connect(mapStateToProps(LogIn));
+export default LogIn;
 /* 
 username
 password
